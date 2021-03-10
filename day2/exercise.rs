7ae2bdb66c1328a6750f8630d32e6955b2a67d97
muduo <aoumeior@outlook.com>
2021-03-10 23:42:01 +0800

    💕

---
+++ day2/exercise.rs
@@ -0,0 +1,47 @@
+struct Person {
+    first: String,
+    middle: Option<String>,
+    last: String,
+}
+
+fn build_full_name(person: &Person) -> String {
+    let mut full_name = String::new();
+    full_name.push_str(&person.first);
+    full_name.push_str(" ");
+
+    let middle = &person.middle.as_ref();
+    full_name.push_str(middle.unwrap_or(&String::from(" ")));
+
+    match middle {
+        None => (),
+        _ => full_name.push_str(" "),
+    }
+    
+
+
+    full_name.push_str(&person.last);
+    full_name
+}
+
+fn main() {
+    let john = Person {
+    first: String::from("James"),
+    middle: Some(String::from("Oliver")),
+    last: String::from("Smith"),
+    };
+    assert_eq!(build_full_name(&john), "James Oliver Smith");
+
+    let alice = Person {
+        first: String::from("Alice"),
+        middle: None,
+        last: String::from("Stevens"),
+    };
+    assert_eq!(build_full_name(&alice), "Alice Stevens");
+
+    let bob = Person {
+        first: String::from("Robert"),
+        middle: Some(String::from("Murdock")),
+        last: String::from("Jones"),
+    };
+    assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
+}
