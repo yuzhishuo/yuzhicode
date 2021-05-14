f625292f9228ac2efa3a08a2cc644622e7c830b9
yimin <yimin.muduo@outlook.com>
2021-05-14 23:34:04 +0800

    fix #4 so easy

--- leetcode/rust/l198/src/main.rs
+++ leetcode/rust/l198/src/main.rs
@@ -1,3 +1,52 @@
+struct Solution;
+
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+        if nums.len() == 1 {
+            return nums[0];
+        }
+        if nums.len() == 2 {
+            return nums[0].max(nums[1]);
+        }
+
+        let mut v = vec![nums[0], nums[0].max(nums[1])];
+        let mut ret = v[0].max(v[1]);
+
+        for i in 2..nums.len() {
+            v.push(v[i - 1].max(v[i - 2] + nums[i]));
+            ret = v[i].max(ret);
+        }
+
+        ret
+    }
+
+    pub fn rob1(nums: Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+        if nums.len() == 1 {
+            return nums[0];
+        }
+        if nums.len() == 2 {
+            return nums[0].max(nums[1]);
+        }
+        let mut nums = nums;
+
+        nums[1] = nums[0].max(nums[1]);
+        let mut ret = nums[1];
+
+        for i in 2..nums.len() {
+            nums[i] = nums[i - 1].max(nums[i - 2] + nums[i]);
+            ret = nums[i].max(ret);
+        }
+
+        ret
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Solution::rob(vec![1, 2, 3, 1]), 4);
 }
