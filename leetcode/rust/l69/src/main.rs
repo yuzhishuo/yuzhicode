018b2504e3df116e851e4261b71fcaa31f58c0df
liuym02 <liuyimin@corp.netease.com>
2021-05-17 21:17:16 +0800

    fix #17 binary search

--- leetcode/rust/l69/src/main.rs
+++ leetcode/rust/l69/src/main.rs
@@ -9,8 +9,26 @@ impl Solution {
         }
         k as i32
     }
+
+    pub fn my_sqrt1(x: i32) -> i32 {
+        let mut r = x;
+        let mut l = 0;
+
+        let mut ans = -1;
+        while l < r {
+            let mid = (l + r) / 2;
+            if mid * mid <= x {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        ans
+    }
 }
 
 fn main() {
     assert_eq!(Solution::my_sqrt(9), 3);
+    assert_eq!(Solution::my_sqrt1(10), 3);
 }
