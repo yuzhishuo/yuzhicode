45eb912940ef0d412bec9244c0d06574fb851c06
muduo <aoumeior@outlook.com>
2021-05-19 19:48:55 +0800

    😍

--- leetcode/rust/l704/src/main.rs
+++ leetcode/rust/l704/src/main.rs
@@ -2,14 +2,16 @@ struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut left = 0 as i32;
+        let mut 
+        
+        left = 0 as i32;
         let mut right = (nums.len() - 1) as i32;
         let mut mid = 0 as i32;
         while left <= right {
             mid = (left + right) / 2;
-            if (nums[mid as usize] == target) {
+            if nums[mid as usize] == target {
                 return mid;
-            } else if (nums[mid as usize] > target) {
+            } else if nums[mid as usize] > target {
                 right = mid - 1;
             } else {
                 left = mid + 1;
