f6495278dd47819c4b2d756cbe0e4567e194e9dc
yuzhishuo <aoumeior@outlook.com>
2021-06-18 00:23:23 +0800

    🌹

---
+++ leetcode/rust-go/r6/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080");
+
+    let listener = listener.unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("connect established");
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
