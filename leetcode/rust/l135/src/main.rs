9491511c3321352b8882c804aa1e2830afcd96e1
yuzhishuo <aoumeior@outlook.com>
2021-07-07 00:20:29 +0800

    fix luluyuzhi#61 l135

--- leetcode/rust/l135/src/main.rs
+++ leetcode/rust/l135/src/main.rs
@@ -1,3 +1,24 @@
+struct Solution;
+impl Solution {
+    pub fn candy(ratings: Vec<i32>) -> i32 {
+        let size = ratings.len();
+        if size < 2 {
+            return size as i32;
+        }
+        let mut num = vec![1; size];
+        for i in 1..size {
+            if ratings[i] > ratings[i - 1] {
+                num[i] = num[i - 1] + 1;
+            }
+        }
+        for i in (1..size).rev() {
+            if ratings[i] < ratings[i - 1] {
+                num[i - 1] = num[i - 1].max(num[i] + 1);
+            }
+        }
+        num.iter().sum::<usize>() as i32
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Solution::candy(vec![1, 0, 2]), 5);
 }
